5280e6ee0a5e71e16fd223f8653a62535cc97604
vikash <vikashraj144@gmail.com>
2019-02-17 12:43:53 +0530

    question added

--- javaScript/javascript-exercise2.js
+++ javaScript/javascript-exercise2.js
@@ -2,3 +2,9 @@ console.log(1 == '1')
 // Its returns true because it's an auto-type conversion and it checks only value not type.
 console.log(1 === '1')
 // Its returns false because it's not auto-type conversion and it check value and type both.
+console.log(1=== parseInt('1'))
+//  its returns true.
+ console.log(0 == false); // return true, because both are same type.
+ console.log(0 === false); // return false, because both are of a different type.
+ console.log(1 == "1"); // return true, automatic type conversion for value only.
+ console.log(1 === "1"); // return false, because both are of a different type.
