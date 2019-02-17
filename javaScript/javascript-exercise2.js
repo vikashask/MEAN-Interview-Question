81d0f7db1f599acb3f57c5491edacc394dfb159f
vikash <vikashraj144@gmail.com>
2019-02-17 12:25:08 +0530

    js question added

---
+++ javaScript/javascript-exercise2.js
@@ -0,0 +1,4 @@
+console.log(1 == '1')
+// Its returns true because it's an auto-type conversion and it checks only value not type.
+console.log(1 === '1')
+// Its returns false because it's not auto-type conversion and it check value and type both.
