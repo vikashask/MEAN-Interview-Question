a3580bef70dddda9b7856c2f6ac5bf1a265875f2
vikash <vikashraj144@gmail.com>
2019-02-17 17:33:31 +0530

    upated question

---
+++ javaScript/2.javascript-exercise.js
@@ -0,0 +1,39 @@
+console.log(1 == '1')
+// Its returns true because it's an auto-type conversion and it checks only value not type.
+console.log(1 === '1')
+// Its returns false because it's not auto-type conversion and it check value and type both.
+console.log(1=== parseInt('1'))
+//  its returns true.
+ console.log(0 == false); // return true, because both are same type.
+ console.log(0 === false); // return false, because both are of a different type.
+ console.log(1 == "1"); // return true, automatic type conversion for value only.
+ console.log(1 === "1"); // return false, because both are of a different type.
+ console.log(null == undefined); // return true.
+ console.log(null === undefined); // return false.
+ console.log('0' == false); // return true.
+ console.log('0' === false); // return false.
+ console.log(1=== parseInt("1")); // return true.
+
+//1.  What is the difference between undefined and not defined in 
+var x; // declaring x
+console.log(x); //output: undefined
+console.log(y);  // Output: ReferenceError: y is not defined
+
+// 2.what would be output
+var y1 = 1;
+  if (function f(){}) {
+    y1 += typeof f;
+  }
+  console.log(y1); 
+/*
+// 1undefined
+eval(function f(){}) returns function f(){} (which is true)   
+if statement, executing typeof f returns undefined because the if statement code executes at run time, 
+and the statement inside the if condition is evaluated during run time.
+*/
+var k = 1;
+  if (1) {
+    function foo(){};
+    k += typeof foo;
+  }
+  console.log(k); // output 1function
