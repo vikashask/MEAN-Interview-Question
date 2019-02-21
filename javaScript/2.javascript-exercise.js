a45fb449015fa2fd92cd63566b178ffa89c7f7b2
vikash <vikashraj144@gmail.com>
2019-02-21 11:53:52 +0530

    question added

--- javaScript/2.javascript-exercise.js
+++ javaScript/2.javascript-exercise.js
@@ -108,3 +108,11 @@ console.log(trees);
 // [ 'redwood', 'bay', 'cedar', <1 empty item>, 'maple' ] in node terminal
 trees.push();
 console.log(trees.length); // 5
+
+/* What would be Output */
+var bar = true;
+console.log(bar + 0);   
+console.log(bar + "xyz");  
+console.log(bar + true);  
+console.log(bar + false);  
+// 1, "truexyz", 2, 1
