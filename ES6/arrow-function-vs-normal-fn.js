44fb036c1955fb5e61f579a6d51904f3d4542ff4
vikash <vikashraj144@gmail.com>
2020-08-31 16:34:57 +0530

    arrow fn vs normal function

---
+++ ES6/arrow-function-vs-normal-fn.js
@@ -0,0 +1,66 @@
+// const Person = function (name, age) {
+//   this.age = age;
+//   this.name = name;
+// }
+
+// Person.prototype.introduction = function () {
+//   const getAge = () => {
+//     return this.age;
+//   }
+//   return `My name is ${this.name} and I am ${getAge()} years old!`;
+// }
+
+// Person.sayHi = function() {
+//   console.log(this);
+// }
+
+// Person.prototype.func2 = () => {
+//   console.log(this);
+// }
+
+// class Person {
+//   constructor(name, age) {
+//     this.age = age;
+//     this.name = name;
+//   }
+
+//   introduction () {
+//     return `My name is ${this.name} and I am ${this.age} years old!`;
+//   }
+
+//   static sayHi() {
+//     console.log('say hi');
+//   }
+// }
+
+const sayNameFunc = function() {
+    console.log(this.name);
+  }
+  
+  const Library = {
+    introduction: function () {
+      console.log(`Hi I am john`);
+    },
+    createInstance: function (name) {
+      this._created = true;
+      this.name = name;
+      return this;
+    },
+    sayName: sayNameFunc
+  }
+  
+  // let john = new Person("John Smith", 18);
+  // console.log(john);
+  // john.func2();
+  
+  const john = Library.createInstance('John');
+  
+  john.introduction();
+  john.sayName();
+  
+  // Person.sayHi();
+  
+  /*
+  1. constructor function
+  2. normal function
+  */
