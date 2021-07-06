bbfcdb0fb2a537f238307f10d36b26c9a2bf63fe
vikash <vikashraj144@gmail.com>
2021-07-06 14:21:37 +0530

    basic q

---
+++ javaScript/js basic interview.js
@@ -0,0 +1,79 @@
+// 1.
+// const foo = [1, 2, 3];
+// const [n] = foo;
+// console.log(n);
+
+// 2.
+// var a = ["dog", "cat", "hen"];
+// a[100] = "fox";
+// console.log(a.length);
+
+// 3. how to call method
+// class TaxCalculator {
+//   static calculate(total) {
+//     return total * 0.05;
+//   }
+// }
+
+// let data = TaxCalculator.calculate(50);
+// console.log("🚀 ~ file: 1.Traversing-an-Array.js ~ line 18 ~ data", data);
+
+// 4.
+// const foo = {
+//   bar(){
+//     console.log("Hello, world!");
+//   },
+//   name: "Albert",
+//   age: 26,
+// }
+// console.log("I");
+// setTimeout(() => {
+//   console.log("love");
+// }, 0);
+// console.log("JavaScript!");
+
+// 5.
+// const baz = () => "Hello, world!";
+// export const foo = {
+//   name: "Albert"
+// };
+// export function bar(){
+//   return 42;
+// }
+// export default baz;
+// export default () => ({});
+
+// 6.
+// const foo = {
+//   name: "Albert",
+// };
+// console.log(
+//   "🚀 ~ file: 1.Traversing-an-Array.js ~ line 56 ~ del.foo.name",
+//   delete foo.name
+// );
+
+// 7.
+// var v = 1;
+// var f1 = function () {
+//   console.log(v);
+// };
+// var f2 = function () {
+//   var v = 2;
+//   f1();
+// };
+// f2();
+
+// 8.
+// const dessert = { type: "pie" };
+// dessert.type = "pudding";
+
+// const seconds = dessert;
+// seconds.type = "fruit";
+// console.log(
+//   "🚀 ~ file: 1.Traversing-an-Array.js ~ line 72 ~ dessert.type",
+//   dessert.type
+// );
+
+// 9.
+var obj;
+console.log(obj);
